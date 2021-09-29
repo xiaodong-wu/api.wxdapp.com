51e3f9e2d53e23ed506513e2686b29e680b2bfc5
wuxiaodong <919323717@qq.com>
2021-09-29 23:15:49 +0800

    Update Index.php

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -6,6 +6,11 @@
     class Index extends Controller
     {
         public function index(){
+
+            if(true){
+                echo 111;
+            }
+
             return 'api/index/index';
         }
 
